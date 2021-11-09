e8f8f5a03234e3789d597c4ed3809257249c6c75
hikaru67 <shinigamii.hikaru@gmail.com>
2021-11-09 07:34:49 +0700

    layout

---
+++ components/layouts/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container-fluid">
+    © 2020
+    <a
+      href="https://hoichaohanh.com/"
+      target="_blank"
+    >HOI CHAO HANH - Adminstrator</a>
+
+    <span>Powered by</span> <a
+      href="https://hoichaohanh.com/"
+      target="_blank"
+    >HOI CHAO HANH</a>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <Header />
+  <!-- end main header -->
+
+  <transition name="fade">
+    <div class="container-fluid">
+      <slot />
+      <!-- end main content -->
+    </div>
+  </transition>
+
+  <Footer />
+  <!-- end main footer -->
+  <!-- end main sidebar -->
+</template>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
+
+<script>
+import Footer from '~/components/layouts/Footer'
+import Header from '~/components/layouts/Header'
+
+export default {
+  components: {
+    Footer,
+    Header
+  },
+
+  middleware: ['auth'],
+
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
