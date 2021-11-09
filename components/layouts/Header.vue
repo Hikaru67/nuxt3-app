d863fddb7312629cca17e77c4c0d9be90209f9a8
quangnn67 <quang.nn@digidinos.com>
2021-11-09 17:30:37 +0700

    header

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -1,76 +1,17 @@
 <template>
-  <div class="container">
-    <div class="navbar-header">
-      <div class="navbar-brand Module"><a
-          class="logo"
-          title="Truyện tranh online"
-          href="/"
-        ><img
-            alt="Logo NetTruyen"
-            src="//st.imageinstant.net/data/logos/logo-nettruyen.png"
-          ></a></div>
-      <div class="navbar-form navbar-left hidden-xs search-box comicsearchbox">
-        <div class="input-group">
-          <input
-            type="text"
-            class="searchinput form-control"
-            placeholder="Tìm truyện..."
-            autocomplete="off"
-          >
-          <div class="input-group-btn">
-            <input
-              type="submit"
-              value=""
-              class="searchbutton btn btn-default"
-            >
-          </div>
-        </div>
-      </div>
+  <header class="header">
+    <div class="container">
       <a
-        rel="nofollow"
-        title="Đăng ký nhận thông báo"
-        href="https://www.nettruyenpro.com/Secure/Notification.aspx"
-        class="notify-btn hidden hide"
+        href="/"
+        class="navbar-brand"
       >
-        <i class="fa fa-bell"></i>
-        <span class="dot">
-          <span class="ping"></span>
-        </span>
-      </a>
-      <div class="notifications">
-        <a
-          href="#"
-          title="Thông báo"
+        <img
+          src="@/assets/images/logo-nettruyen.png"
+          alt=""
         >
-          <i class="fa fa-comment" />
-        </a>
-      </div><button
-        type="button"
-        class="search-button-icon visible-xs"
-        aria-label="Search"
-      >
-        <i class="fa fa-search"></i>
-      </button>
-      <button
-        type="button"
-        class="navbar-toggle"
-        aria-label="Menu"
-      >
-        <i class="fa fa-bars"></i>
-      </button>
+      </a>
     </div>
-    <ul class="nav-account list-inline hidden-xs pull-right">
-      <li class="login-link"><a
-          rel="nofollow"
-          href="https://www.nettruyenpro.com/Secure/Login.aspx?returnurl=%2F"
-        >Đăng nhập</a></li>
-      <li class="register-link"><a
-          rel="nofollow"
-          href="https://www.nettruyenpro.com/Secure/Register.aspx?returnurl=%2F"
-        >Đăng ký</a>
-      </li>
-    </ul>
-  </div>
+  </header>
 </template>
 
 <style scoped lang="scss" />
