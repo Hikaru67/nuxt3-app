56c41e65588ac6283f00824cd63158fc583396f0
quangnn67 <quang.nn@digidinos.com>
2021-11-12 17:23:03 +0700

    add slider

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -1,31 +1,33 @@
 <template>
   <header class="header">
-    <div class="container flex">
-      <div class="nav-brand">
-        <a
-          href="/"
-          class="navbar-brand"
-        >
-          <img
-            src="@/assets/images/logo-nettruyen.png"
-            alt=""
+    <div class="container h-full">
+      <div class="flex space-x-3 h-full items-center">
+        <div class="nav-brand">
+          <a
+            href="/"
+            class="navbar-brand"
           >
-        </a>
-      </div>
-      <div class="nav-form">
-        <div class="input-group">
-          <input
-            type="text"
-            class="searchinput form-control"
-            placeholder="Tìm truyện..."
-            autocomplete="off"
-          >
-          <div class="input-group-btn">
-            <input
-              type="submit"
-              value=""
-              class="searchbutton btn btn-default"
+            <img
+              src="@/assets/images/logo-nettruyen.png"
+              alt=""
             >
+          </a>
+        </div>
+        <div class="nav-form">
+          <div class="input-group">
+            <input
+              type="text"
+              class="searchinput form-control"
+              placeholder="Tìm truyện..."
+              autocomplete="off"
+            >
+            <div class="input-group-btn">
+              <input
+                type="submit"
+                value=""
+                class="searchbutton btn btn-default"
+              >
+            </div>
           </div>
         </div>
       </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,153 @@
 <template>
-  asdas
+  <main>
+    <div class="container">
+      <div
+        id="ctl00_divAlt1"
+        class="altcontent1 cmszone"
+      >
+        <div class="top-comics Module Module-183">
+          <div class="ModuleContent">
+            <h2 class="page-title">Truyện đề cử <i class="fa fa-angle-right"></i></h2>
+            <div class="items-slide">
+              <div
+                class="owl-carousel clearfix owl-theme"
+                style="opacity: 1; display: block;"
+              >
+                <div class="owl-wrapper-outer">
+                  <div
+                    class="owl-wrapper"
+                    style="width: 4060px; left: 0px; display: block; transition: all 1000ms ease 0s; transform: translate3d(0px, 0px, 0px);"
+                  >
+                    <div
+                      class="owl-item"
+                      style="width: 203px;"
+                    >
+                      <div class="item">
+                        <a href="http://www.nettruyenpro.com/truyen-tranh/chien-hon-tuyet-the-30951">
+                          <img
+                            class="lazyOwl"
+                            src="//st.imageinstant.net/data/comics/231/chien-hon-tuyet-the.jpg"
+                            alt="Chiến Hồn Tuyệt Thế"
+                            style="display: inline;"
+                          >
+                        </a>
+                        <div class="slide-caption">
+                          <h3>
+                            <a
+                              href="http://www.nettruyenpro.com/truyen-tranh/chien-hon-tuyet-the-30951"
+                              title="Chiến Hồn Tuyệt Thế"
+                            >Chiến Hồn Tuyệt Thế</a>
+                          </h3>
+                          <a
+                            href="http://www.nettruyenpro.com/truyen-tranh/chien-hon-tuyet-the/chap-210/786358"
+                            title="Chapter 210"
+                          >Chapter 210</a>
+                          <span class="time">
+                            <i class="fa fa-clock-o">
+                            </i> 18 giờ trước</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    class="owl-wrapper"
+                    style="width: 4060px; left: 0px; display: block; transition: all 1000ms ease 0s; transform: translate3d(0px, 0px, 0px);"
+                  >
+                    <div
+                      class="owl-item"
+                      style="width: 203px;"
+                    >
+                      <div class="item">
+                        <a href="http://www.nettruyenpro.com/truyen-tranh/chien-hon-tuyet-the-30951">
+                          <img
+                            class="lazyOwl"
+                            src="//st.imageinstant.net/data/comics/231/chien-hon-tuyet-the.jpg"
+                            alt="Chiến Hồn Tuyệt Thế"
+                            style="display: inline;"
+                          >
+                        </a>
+                        <div class="slide-caption">
+                          <h3>
+                            <a
+                              href="http://www.nettruyenpro.com/truyen-tranh/chien-hon-tuyet-the-30951"
+                              title="Chiến Hồn Tuyệt Thế"
+                            >Chiến Hồn Tuyệt Thế</a>
+                          </h3>
+                          <a
+                            href="http://www.nettruyenpro.com/truyen-tranh/chien-hon-tuyet-the/chap-210/786358"
+                            title="Chapter 210"
+                          >Chapter 210</a>
+                          <span class="time">
+                            <i class="fa fa-clock-o">
+                            </i> 18 giờ trước</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    class="owl-wrapper"
+                    style="width: 4060px; left: 0px; display: block; transition: all 1000ms ease 0s; transform: translate3d(0px, 0px, 0px);"
+                  >
+                    <div
+                      class="owl-item"
+                      style="width: 203px;"
+                    >
+                      <div class="item">
+                        <a href="http://www.nettruyenpro.com/truyen-tranh/chien-hon-tuyet-the-30951">
+                          <img
+                            class="lazyOwl"
+                            src="//st.imageinstant.net/data/comics/231/chien-hon-tuyet-the.jpg"
+                            alt="Chiến Hồn Tuyệt Thế"
+                            style="display: inline;"
+                          >
+                        </a>
+                        <div class="slide-caption">
+                          <h3>
+                            <a
+                              href="http://www.nettruyenpro.com/truyen-tranh/chien-hon-tuyet-the-30951"
+                              title="Chiến Hồn Tuyệt Thế"
+                            >Chiến Hồn Tuyệt Thế</a>
+                          </h3>
+                          <a
+                            href="http://www.nettruyenpro.com/truyen-tranh/chien-hon-tuyet-the/chap-210/786358"
+                            title="Chapter 210"
+                          >Chapter 210</a>
+                          <span class="time">
+                            <i class="fa fa-clock-o">
+                            </i> 18 giờ trước</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div
+                  class="owl-controls clickable"
+                  style="display: block;"
+                >
+                  <div class="owl-pagination">
+                    <div class="owl-page active"><span class=""></span></div>
+                    <div class="owl-page"><span class=""></span></div>
+                  </div>
+                </div>
+              </div>
+              <a
+                href="#"
+                class="prev"
+                aria-label="Trước"
+              >
+              </a>
+              <a
+                href="#"
+                class="next"
+                aria-label="Sau"
+              >
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </main>
 </template>
